9da55fec52ad1d0ac7811dbb0155574bcb4e78dc
augustine0815 <109200074+augustine0815@users.noreply.github.com>
2022-07-14 17:38:26 +0800

    first commit

---
+++ app/Controllers/Helloworld.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+class Helloworld extends BaseController
+{
+    public function getIndex()
+    {
+        return 'Hello World!';
+    }
+    public function getComment()
+    {
+        return 'I am not flat!';
+    }
+}
